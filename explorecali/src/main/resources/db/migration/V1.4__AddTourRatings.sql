4a0df27f37d0847c8d894302b96fc9314919b75e
Silvio Torre <54410963+silviotorre@users.noreply.github.com>
2021-10-23 09:46:24 +0200

    minor fix

--- explorecali/src/main/resources/db/migration/V1.4__AddTourRatings.sql
+++ explorecali/src/main/resources/db/migration/V1.4__AddTourRatings.sql
@@ -5,7 +5,7 @@
   (1, 6, 5, 'She loved it'),
   (1, 7, 5, 'He loved it'),
   (1, 8, 5, 'They loved it'),
-  (1, 9, 5, 'Everbody loved it'),
+  (1, 9, 5, 'Nobody loved it'),
   (1, 10, 1, 'I hated it'),
   (1, 11, 1, 'We hated it'),
   (2, 100, 5, 'I really thought it could have been better');
